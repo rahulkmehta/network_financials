9b92a03aabe198c2a903a131c440aa5bdd76e8b2
rahulkmehta <rkmehta@usc.edu>
2020-06-24 13:43:49 -0700

    fixes

--- webscraper.py
+++ webscraper.py
@@ -17,6 +17,20 @@ import argparse
 import traceback
 import json
 from bs4 import BeautifulSoup
+from boxsdk import OAuth2, Client
+import ast
+import pickle
+
+auth = OAuth2(
+    client_id = 'l1je8xajdp77h9f8eu2bs27w6a2c2kcf',
+    client_secret = 'T1IFvFU4Pv6Dz6gDB8Lf8LujnpIxJsJr',
+    access_token = 'CYf8dCFSNQ91UjAj1ATZ1lgbAQaRDv70',
+)
+client = Client(auth)
+
+year_dict = {'2019': ['al', 'ar', 'az', 'co', 'fl', 'ga', 'ia', 'id', 'il', 'ks', 'la', 'mn', 'mt', 'nd', 'nj', 'nv', 'ny', 'oh', 'ok', 'pa', 'sc', 'tx', 'wa', 'wy'],
+ '2018': ['ca', 'ct', 'de', 'in', 'ky', 'ma', 'md', 'me', 'mi', 'mo', 'ms', 'mt', 'nc', 'nd', 'ne', 'nh', 'nm', 'or', 'ri', 'sd', 'tn', 'ut', 'va', 'vt', 'wi', 'wv'],
+ '2017': ['al', 'ar', 'az', 'co', 'de', 'fl', 'ga', 'ia', 'id', 'il', 'ks', 'la', 'md', 'mn', 'mt', 'nd', 'nj', 'nv', 'ny', 'oh', 'ok', 'pa', 'sc', 'wa', 'wi', 'wy']}
 
 #[SCRAPE FROM NAIP DATABASE]
 def absorbdataintoarray():
@@ -66,26 +80,147 @@ def scrape_stores():
 
 #[CROSS-REFERENCE COUNTIES WITH STORES' ADDRESS AND SAVE TO FILE]
 def filter_stores(dictionary):
-	file = open("reducedlist.txt", "w")
-	with open('stores.txt') as json_file:
+	def_list = []
+	with open('extra_data/stores.txt') as json_file:
 		data = json.load(json_file)
 		for k,v in data.items():
 			for key, value in dictionary.items():
 				temp_city_holder = v[1].upper()
 				temp_city_holder = temp_city_holder.replace("SAINT", "ST")
 				if (temp_city_holder == value[0].upper() and v[2].upper() == key[0].upper()):
-					file.write(str((v[0].upper(), key[0], key[1])))
-					file.write('\n')
-	file.close()
+					def_list.append((str(v[0].upper()), key[0], key[1]))
+	with open('filteredstorelist.pickle', 'wb') as f:
+		pickle.dump(def_list, f)
 
 #[SCRAPE DICTIONARY OF AVAILABLE DATA FROM NAIP]
-def scrape_naip():
-	page = requests.get("https://nrcs.app.box.com/v/naip/folder/17936490251")
-	soup = BeautifulSoup(page.content, 'html.parser')
-	for item in soup.find("div", {"class": "ReactVirtualized__Table__headerRow table-header"}):
-		print (item)
+def scrape_naip(c19, c18, c17):
+	fsl = []
+	state_breakdown = {'2019': [], '2018': [], '2017': []}
+	finalbreakdown_withcountycrossref = {'2019': [], '2018': [], '2017': []}
+	with open ('filteredstorelist.pickle', 'rb') as f:
+		data = pickle.load(f)
+		for item in data:
+			if (item[1].lower() in year_dict['2019']):
+				state_breakdown['2019'].append(item)
+			if (item[1].lower() in year_dict['2018']):
+				state_breakdown['2018'].append(item)
+			if (item[1].lower() in year_dict['2017']):
+				state_breakdown['2017'].append(item)
+
+	for k,v in state_breakdown.items():
+		for item in v:
+			if k == '2019':
+				if int(item[2]) in c19[item[1].lower()]:
+					finalbreakdown_withcountycrossref['2019'].append((item[0], item[1], item[2]))
+			elif k == '2018':
+				if int(item[2]) in c18[item[1].lower()]:
+					finalbreakdown_withcountycrossref['2018'].append((item[0], item[1], item[2]))
+			else:
+				if int(item[2]) in c17[item[1].lower()]:
+					finalbreakdown_withcountycrossref['2017'].append((item[0], item[1], item[2]))
+	return finalbreakdown_withcountycrossref
+
+def populate_county_dictionary():
+	county2019 = {}
+	for item in year_dict['2019']:
+		county2019[item] = []
+
+	county2018 = {}
+	for item in year_dict['2018']:
+		county2018[item] = []
+
+	county2017 = {}
+	for item in year_dict['2017']:
+		county2017[item] = []
+
+	#[2019]
+	county2019['al'] = list(range(1, 134, 2))
+	county2019['ar'] = list(range(1, 150, 2))
+	county2019['az'] = list(range(1, 28, 2))
+	county2019['az'].append(12)
+	county2019['co'] = list(range(1, 128, 2))
+	county2019['fl'] = list(range(1, 135, 2))
+	county2019['ga'] = list(range(1, 322, 2))
+	county2019['ia'] = list(range(1, 198, 2))
+	county2019['id'] = list(range(1, 89, 2))
+	county2019['il'] = list(range(1, 205, 2))
+	county2019['ks'] = list(range(1, 211, 2))
+	county2019['la'] = list(range(1, 129, 2))
+	county2019['mn'] = list(range(1, 175, 2))
+	county2019['mt'] = list(range(1, 99, 2))
+	county2019['nd'] = list(range(1, 107, 2))
+	county2019['nj'] = list(range(1, 42, 2))
+	county2019['nv'] = list(range(1, 33, 2))
+	county2019['oh'] = list(range(1, 175, 2))
+	county2019['ok'] = list(range(1, 154, 2))
+	county2019['pa'] = list(range(1, 135, 2))
+	county2019['sc'] = list(range(1, 93, 2))
+	county2019['tx'] = list(range(1, 511, 2))
+	county2019['wa'] = list(range(1, 79, 2))
+	county2019['wy'] = list(range(1, 47, 2))
+
+	#[2018]	
+	county2018['ca'] = list(range(1, 117, 2))
+	county2018['ct'] = list(range(1, 17, 2))
+	county2018['de'] = list(range(1, 6, 2))
+	county2018['in'] = list(range(1, 186, 2))
+	county2018['ky'] = list(range(1, 241, 2))
+	county2018['ma'] = list(range(1, 29, 2))
+	county2018['md'] = list(range(1, 49, 2))
+	county2018['me'] = list(range(1, 33, 2))
+	county2018['mi'] = list(range(1, 167, 2))
+	county2018['mo'] = list(range(1, 231, 2))
+	county2018['ms'] = list(range(1, 165, 2))
+	county2018['mt'] = list(range(1, 27, 2))
+	county2018['nc'] = list(range(1, 201, 2))
+	county2018['nd'] = list(range(1, 107, 2))
+	county2018['ne'] = list(range(1, 187, 2))
+	county2018['nh'] = list(range(1, 21, 2))
+	county2018['nm'] = list(range(1, 67, 2))
+	county2018['or'] = list(range(1, 97, 2))
+	county2018['ri'] = list(range(1, 11, 2))
+	county2018['sd'] = list(range(1, 132, 2))
+	county2018['tn'] = list(range(1,191, 2))
+	county2018['ut'] = list(range(1, 59, 2))
+	county2018['va'] = list(range(1, 201, 2))
+	county2018['vt'] = list(range(1, 29, 2))
+	county2018['wi'] = list(range(1, 145, 2))
+	county2018['wv'] = list(range(1, 111, 2))
+
+	#[2017]	
+	county2017['al'] = list(range(1, 134, 2))
+	county2017['ar'] = list(range(1, 150, 2))
+	county2017['az'] = list(range(1, 28, 2))
+	county2017['co'] = list(range(1, 126, 2))
+	county2017['de'] = list(range(1, 6, 2))
+	county2017['fl'] = list(range(1, 134, 2))
+	county2017['ga'] = list(range(1, 318, 2))
+	county2017['ia'] = list(range(1, 198, 2))
+	county2017['id'] = list(range(1, 88, 2))
+	county2017['il'] = list(range(1, 204, 2))
+	county2017['ks'] = list(range(1, 210, 2))
+	county2017['la'] = list(range(1, 128, 2))
+	county2017['md'] = list(range(1, 48, 2))
+	county2017['mn'] = list(range(1, 174, 2))
+	county2017['mt'] = list(range(1, 110, 2))
+	county2017['nd'] = list(range(1, 106, 2))
+	county2017['nj'] = list(range(1, 42, 2))
+	county2017['nv'] = list(range(1, 34, 2))
+	county2017['ny'] = list(range(1, 124, 2))
+	county2017['oh'] = list(range(1, 176, 2))
+	county2017['ok'] = list(range(1, 154, 2))
+	county2017['pa'] = list(range(1, 134, 2))
+	county2017['sc'] = list(range(1, 92, 2))
+	county2017['wa'] = list(range(1, 78, 2))
+	county2017['wi'] = list(range(1, 142, 2))
+	county2017['wy'] = list(range(1, 46, 2))
+
+	return county2019, county2018, county2017
 
 def testmain():
-	scrape_naip()
-	
+	c19, c18, c17 = populate_county_dictionary()
+	finalbeforers = scrape_naip(c19, c18, c17)
+	for k,v in finalbeforers.items():
+		print (len(v))
+
 testmain()
